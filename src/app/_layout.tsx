f16fbcca79b1d22331185ae96b05a07f6f16116b
heayem <heayem7@gmail.com>
2026-06-05 07:58:29 +0700

    feat: implement root layout with custom animated splash screen and remove expo-splash-screen dependency

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,29 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { DarkTheme, DefaultTheme, ThemeProvider, Stack } from 'expo-router';
 import { useColorScheme } from 'react-native';
-import * as ExpoSplashScreen from 'expo-splash-screen';
 
 import SplashScreen from '@/features/splash/components/splash-screen';
-
-ExpoSplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [showSplash, setShowSplash] = useState(true);
-
-  useEffect(() => {
-    const init = async () => {
-      try {
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        await ExpoSplashScreen.hideAsync();
-      } catch (error) {
-        console.error(error);
-        await ExpoSplashScreen.hideAsync();
-      }
-    };
-
-    init();
-  }, []);
 
   return (
     <ThemeProvider
